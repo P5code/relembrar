4e93557edc965e376db3260b0bed64c85756cef8
P5code <161071252+P5code@users.noreply.github.com>
2024-03-12 20:04:55 -0300

    Create codigos_das_aulas.c

---
+++ codigos_das_aulas.c
@@ -0,0 +1,70 @@
+/*#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+  printf("hello world\n");
+  printf("how are the time\n\n");
+  system("pause");
+
+  int x;
+
+  printf("mendiga m numero e eu digo ele de volta\n");
+  scanf("%d",&x);
+  printf("%d\n",x);
+  system("pause");
+
+  int n1,n2,n3;
+
+  printf("digite um numero\n");
+  scanf("%d",&n1);
+  printf("me diga outro\n");
+  scanf("%d",&n2);
+
+  n3= n1 + n2;
+  printf("a soma dos dois numeros e: %d\n",n3);
+
+  system("pause");
+  
+
+}*/
+
+/*#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	
+	float n1,n2,n3,n4,n5;
+	
+	printf("Me de um numero\n");
+	scanf("%f",&n1);
+	
+	printf("Me de outro\n");
+	scanf("%f",&n2);
+	
+	n3 = n1 * n2;
+	
+	printf("a multiplicaçao dos numeros e: %f\n",n3);
+	
+	printf("agora me de um numero para dividir este\n");
+	scanf("%f",&n4);
+	
+	n5 = n3 / n4;
+	
+	printf("o resultado e: %f\n",n5);
+	
+	system("pause");
+	
+}*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	
+	char nome[10];
+	
+	scanf("%s",&nome);
+	printf("%s",nome);
+
+}
